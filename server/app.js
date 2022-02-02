4960b902a4509223ca5db461afb8e61c78eab60a
csernakakos <csernakakos@gmail.com>
2022-02-02 14:18:27 +0100

    Complete Lectures 1 to 19

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const {graphqlHTTP} = require("express-graphql");
 const schema = require("./schema/schema");
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
+const db = "mongodb+srv://akos:12345@cluster0.vv4zp.mongodb.net/graphql?retryWrites=true&w=majority";
+
+mongoose.connect(db);
+mongoose.connection.once("open", () => {
+    console.log("connected")
+})
 
 const app = express();
 
 app.use("/graphql", graphqlHTTP({
     schema,
-    graphiql: true,
+    graphiql: true
 }));
 
 app.listen(4000, () => {console.log("listening...")});
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,22 +1,9 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const Book = require("../models/book");
+const Author = require("../models/author");
 
-const myBooks = [
-    {name: "Rev Road", genre: "Drama", id: "1", authorId: "1"},
-    {name: "The Leftovers", genre: "Drama", id: "2", authorId: "2"},
-    {name: "Atonement", genre: "Theatre", id: "3", authorId: "3"},
-    {name: "The Leftovers II", genre: "Drama", id: "4", authorId: "2"},
-    {name: "The Other Book", genre: "Drama", id: "5", authorId: "2"},
-    {name: "Sunday", genre: "Theatre", id: "6", authorId: "2"},
-];
-
-const myAuthors = [
-    {name: "Terry P", age: 26, id: "1"},
-    {name: "Brandon S", age: 44, id: "2"},
-    {name: "Patrik R", age: 77, id: "3"},
-]
-
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLList, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: "Book",
@@ -43,7 +30,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (parent, args) => {
+                // return _.filter(myBooks, { authorId: parent.id});
+            }
+        }
     })
 });
 
@@ -54,21 +47,75 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,     
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-               return _.find(myBooks, {id: args.id});
+            //    return _.find(myBooks, {id: args.id});
             }
         },
         author: {
             type: AuthorType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-                return _.find(myAuthors, {id: args.id});
+                // return _.find(myAuthors, {id: args.id});
+            }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                // return myBooks;
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                // return myAuthors;
             }
         }
     }
 });
 
+// CRUD operations for our database:
+const Mutations = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            // What we expect to receive from the client:
+            args: {
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parent, args) {
+                // new object based on our mongoose model:
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+
+                // save new object to database:
+                return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+
+                return book.save();
+            }
+        }
+    }
+})
+
 // Create new schema and define which query will be the start query.
 module.exports = new GraphQLSchema ({
     query: RootQuery,
+    mutation: Mutations,
 })
-
